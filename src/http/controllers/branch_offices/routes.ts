137d798ae948081e65613dc336a35592a8b0d628
Carlos Cesar Baratella Junior <molusko.rp@hotmail.com>
2024-05-24 18:25:11 -0300

    a

--- src/http/controllers/branch_offices/routes.ts
+++ src/http/controllers/branch_offices/routes.ts
@@ -1,6 +1,16 @@
-import { FastifyInstance } from 'fastify'
+import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
 import { create } from './create'
+import { prisma } from '@/lib/prisma'
 
 export async function branchOfficeRoutes(app: FastifyInstance) {
   app.post('/branch_office', create)
+
+  app.get(
+    '/branch_office',
+    async (req: FastifyRequest, reply: FastifyReply) => {
+      const offices = await prisma.branch_office.findMany()
+
+      reply.send({ offices })
+    },
+  )
 }
